502abdae8c9df146e175f24afb23767d1f1ac84a
VDV777 <60316206+VDV777@users.noreply.github.com>
2024-12-22 20:13:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,118 @@
+import unittest
+from unittest import skipIf
+import logging
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='runner_tests.log',
+    filemode='w',
+    encoding='utf-8',
+    format='%(levelname)s: %(message)s'
+)
+
+
+class Runner_2:
+    def __init__(self, name, speed=5):
+        if not isinstance(name, str):
+            raise TypeError("name должен быть строкой")
+        if speed < 0:
+            raise ValueError("speed должен быть неотрицательным")
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        try:
+            runner = Runner_2("TestRunner", speed=-5)
+            for _ in range(10):
+                runner.walk()
+            self.assertEqual(runner.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning("Неверная скорость для Runner: %s", e)
+
+    @skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        try:
+            runner = Runner_2(12345)
+            for _ in range(10):
+                runner.run()
+            self.assertEqual(runner.distance, 100)
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.warning("Неверный тип данных для объекта Runner: %s", e)
+
+
+if __name__ == "__main__":
+    unittest.main()
